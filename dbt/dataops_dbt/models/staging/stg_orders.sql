3dcdfcb869ba24a2f1d3f163e8e88f97a99aaf2e
yassine <elasriyassine000@gmail.com>
2026-07-18 16:28:20 +0100

    feat(dbt): initialize dbt project and create staging models

---
+++ dbt/dataops_dbt/models/staging/stg_orders.sql
@@ -0,0 +1,8 @@
+SELECT
+    order_id,
+    customer_id,
+    order_status,
+    order_purchase_timestamp,
+    order_delivered_customer_date,
+    order_estimated_delivery_date
+FROM {{ source('bronze','bronze_orders') }}
